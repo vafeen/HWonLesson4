498aed7dc526914c000c91d40035caebf3577dc3
Vafeen <666av6@gmail.com>
2024-04-13 05:48:40 +0300

    Fix bug in MainActivity and add switching colors

--- app/src/main/java/ru/vafeen/hwonlesson4/MainActivity.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/MainActivity.kt
@@ -1,11 +1,15 @@
 package ru.vafeen.hwonlesson4
 
+
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ru.vafeen.hwonlesson4.databinding.ActivityMainBinding
+import ru.vafeen.hwonlesson4.noui.logExecutor
+import ru.vafeen.hwonlesson4.ui.TabRowNaming
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,14 +20,63 @@ class MainActivity : AppCompatActivity() {
 
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
+
+
+        binding.upcoming.setOnClickListener {
+            switchColorsInTabs(TabRowNaming.Upcoming)
+        }
+
+        binding.launchers.setOnClickListener {
+            switchColorsInTabs(TabRowNaming.Launchers)
+        }
+
+        binding.rockets.setOnClickListener {
+            switchColorsInTabs(TabRowNaming.Rockets)
+
+        }
         content()
     }
 
+    private fun switchColorsInTabs(current: TabRowNaming) {
+        logExecutor(mes = "switchColors")
+
+        when (current) {
+            TabRowNaming.Upcoming -> {
+                binding.upcoming.setTextColor(resources.getColor(R.color.red))
+                binding.launchers.setTextColor(resources.getColor(R.color.black))
+                binding.rockets.setTextColor(resources.getColor(R.color.black))
+            }
+
+            TabRowNaming.Launchers -> {
+
+                binding.upcoming.setTextColor(resources.getColor(R.color.black))
+                binding.launchers.setTextColor(resources.getColor(R.color.red))
+                binding.rockets.setTextColor(resources.getColor(R.color.black))
+
+            }
+
+            TabRowNaming.Rockets -> {
+                binding.upcoming.setTextColor(resources.getColor(R.color.black))
+                binding.launchers.setTextColor(resources.getColor(R.color.black))
+                binding.rockets.setTextColor(resources.getColor(R.color.red))
+            }
+
+
+        }
+    }
+
     private fun content() {
+
+//        val listView = findViewById<View>(R.id.myListView) as ListView
+//        val adapter = MyAdapter(this, myData)
+//        listView.setAdapter(adapter)
+
+        binding.apply {
+
+        }
 
     }
 
