1202f5bf7c0f0b9761a2bf89a7dab070670fe061
Vafeen <666av6@gmail.com>
2024-04-14 03:56:33 +0300

    Update class Launch

--- app/src/main/java/ru/vafeen/hwonlesson4/noui/Launch.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/noui/Launch.kt
@@ -1,7 +1,10 @@
 package ru.vafeen.hwonlesson4.noui
 
+import androidx.annotation.LayoutRes
+
 data class Launch(
     val name: String,
     val model: String,
-    val dateStart: String
+    val dateStart: String,
+    @LayoutRes val image: Int
 )
