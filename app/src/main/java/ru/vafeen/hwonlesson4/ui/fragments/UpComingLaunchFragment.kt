561c349147c3b341b8a32927e8c18e89dc531778
Vafeen <666av6@gmail.com>
2024-04-14 06:44:40 +0300

    Add upComingLaunchFragment

--- app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/UpComingLaunchFragment.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/UpComingLaunchFragment.kt
@@ -1,11 +1,9 @@
 package ru.vafeen.hwonlesson4.ui.fragments
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import ru.vafeen.hwonlesson4.R
 import ru.vafeen.hwonlesson4.databinding.LaunchBinding
@@ -13,9 +11,7 @@ import ru.vafeen.hwonlesson4.noui.logExecutor
 import ru.vafeen.hwonlesson4.ui.launch.Launch
 import ru.vafeen.hwonlesson4.ui.launch.LaunchPutGet
 
-
-class LaunchFragment : Fragment(R.layout.launch) {
-
+class UpComingLaunchFragment : Fragment(R.layout.launch) {
     private lateinit var binding: LaunchBinding
 
     override fun onCreateView(
@@ -27,7 +23,7 @@ class LaunchFragment : Fragment(R.layout.launch) {
 
         val data = arguments?.getSerializable(LaunchPutGet.LaunchKey.key) as? Launch
 
-        logExecutor(mes = "data = ${data}") // сюда приходят данные!!
+        logExecutor(mes = "data = $data") // сюда приходят данные!!
 
         binding.apply {
 
@@ -44,5 +40,4 @@ class LaunchFragment : Fragment(R.layout.launch) {
 
         return inflater.inflate(R.layout.launch, container, false)
     }
-
 }
