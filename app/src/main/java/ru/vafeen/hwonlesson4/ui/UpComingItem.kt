6dadb07f3822dd3fc8770db31d215595d62474cd
Vafeen <666av6@gmail.com>
2024-04-14 06:43:13 +0300

    Add upComingItem

---
+++ app/src/main/java/ru/vafeen/hwonlesson4/ui/UpComingItem.kt
@@ -0,0 +1,8 @@
+package ru.vafeen.hwonlesson4.ui
+
+import java.io.Serializable
+
+data class UpComingItem(
+    val title: String,
+    val text: String,
+) : Serializable
---
+++ app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/UpComingItemFragment.kt
@@ -0,0 +1,43 @@
+package ru.vafeen.hwonlesson4.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.vafeen.hwonlesson4.R
+import ru.vafeen.hwonlesson4.databinding.LaunchBinding
+import ru.vafeen.hwonlesson4.noui.logExecutor
+import ru.vafeen.hwonlesson4.ui.launch.Launch
+import ru.vafeen.hwonlesson4.ui.launch.LaunchPutGet
+
+class UpComingItemFragment : Fragment(R.layout.upcoming_info_item) {
+    private lateinit var binding: LaunchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = LaunchBinding.inflate(layoutInflater)
+
+        val data = arguments?.getSerializable(LaunchPutGet.LaunchKey.key) as? Launch
+
+        logExecutor(mes = "data = $data") // сюда приходят данные!!
+
+        binding.apply {
+
+            if (data != null) {
+                name.text = data.name
+
+                model.text = data.model
+
+                start.text = data.dateStart
+
+                icon.setImageResource(data.image)
+            }
+        }
+
+        return inflater.inflate(R.layout.launch, container, false)
+    }
+}
