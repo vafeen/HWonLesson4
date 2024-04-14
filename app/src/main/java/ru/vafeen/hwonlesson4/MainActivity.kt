b8f8917ddccb0b1d8032e966fc3c5e1b8a8d0e33
Vafeen <666av6@gmail.com>
2024-04-14 05:14:02 +0300

    Add LaunchFragment

--- app/src/main/java/ru/vafeen/hwonlesson4/MainActivity.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/MainActivity.kt
@@ -2,18 +2,37 @@ package ru.vafeen.hwonlesson4
 
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import ru.vafeen.hwonlesson4.databinding.ActivityMainBinding
 import ru.vafeen.hwonlesson4.databinding.LaunchBinding
+import ru.vafeen.hwonlesson4.ui.launch.Launch
 import ru.vafeen.hwonlesson4.noui.logExecutor
 import ru.vafeen.hwonlesson4.ui.TabRowNaming
+import ru.vafeen.hwonlesson4.ui.fragments.LaunchFragment
+import ru.vafeen.hwonlesson4.ui.launch.LaunchPutGet
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingMainActivity: ActivityMainBinding
     private lateinit var bindingLaunchBinding: LaunchBinding
+
+    private val launchFragment = LaunchFragment().apply {
+        arguments = Bundle().apply {
+            putSerializable(
+                LaunchPutGet.LaunchKey.key, Launch(
+                    name = "Artur",
+                    model = "Vafin",
+                    dateStart = "today",
+                    image = R.drawable.falcon09
+                )
+            )
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,22 +43,34 @@ class MainActivity : AppCompatActivity() {
 
         bindingLaunchBinding = LaunchBinding.inflate(layoutInflater)
 
-
-
         setContentView(bindingMainActivity.root)
+
+
+
+        replaceFragmentOn(launchFragment)
+
 
         content()
     }
 
-    private fun switchColorsInTabs(current: TabRowNaming) {
+    private fun switchTabs(current: TabRowNaming) {
         logExecutor(mes = "switchColors")
 
         when (current) {
             TabRowNaming.Upcoming -> {
-                bindingMainActivity.upcoming.setTextColor(resources.getColor(R.color.red))
-                bindingMainActivity.launchers.setTextColor(resources.getColor(R.color.black))
-                bindingMainActivity.rockets.setTextColor(resources.getColor(R.color.black))
+                bindingMainActivity.apply {
+                    upcoming.setTextColor(resources.getColor(R.color.red))
+
+                    launchers.setTextColor(resources.getColor(R.color.black))
+
+                    rockets.setTextColor(resources.getColor(R.color.black))
+
+                }
+                bindingLaunchBinding.changeVisibility(View.VISIBLE)
+
+
             }
+
 
             TabRowNaming.Launchers -> {
 
@@ -47,35 +78,62 @@ class MainActivity : AppCompatActivity() {
                 bindingMainActivity.launchers.setTextColor(resources.getColor(R.color.red))
                 bindingMainActivity.rockets.setTextColor(resources.getColor(R.color.black))
 
+                bindingLaunchBinding.changeVisibility(View.GONE)
+
             }
 
             TabRowNaming.Rockets -> {
                 bindingMainActivity.upcoming.setTextColor(resources.getColor(R.color.black))
                 bindingMainActivity.launchers.setTextColor(resources.getColor(R.color.black))
                 bindingMainActivity.rockets.setTextColor(resources.getColor(R.color.red))
+
+                bindingLaunchBinding.changeVisibility(View.GONE)
+
             }
 
 
         }
     }
 
+    private fun LaunchBinding.changeVisibility(visibility: Int) {
+        apply {
+            icon.visibility = visibility
+
+            name.visibility = visibility
+
+            start.visibility = visibility
+
+            launch.visibility = visibility
+        }
+    }
+
+    private fun replaceFragmentOn(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .commit()
+
+
+    }
+
+
     private fun content() {
 
 //        val listView = findViewById<View>(R.id.myListView) as ListView
 //        val adapter = MyAdapter(this, myData)
 //        listView.setAdapter(adapter)
+        switchTabs(TabRowNaming.Upcoming)
 
         bindingMainActivity.apply {
             upcoming.setOnClickListener {
-                switchColorsInTabs(TabRowNaming.Upcoming)
+                switchTabs(TabRowNaming.Upcoming)
             }
 
             launchers.setOnClickListener {
-                switchColorsInTabs(TabRowNaming.Launchers)
+                switchTabs(TabRowNaming.Launchers)
             }
 
             rockets.setOnClickListener {
-                switchColorsInTabs(TabRowNaming.Rockets)
+                switchTabs(TabRowNaming.Rockets)
 
             }
         }
--- app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/LaunchFragment.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/LaunchFragment.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import ru.vafeen.hwonlesson4.R
 import ru.vafeen.hwonlesson4.databinding.LaunchBinding
+import ru.vafeen.hwonlesson4.noui.logExecutor
+import ru.vafeen.hwonlesson4.ui.launch.Launch
+import ru.vafeen.hwonlesson4.ui.launch.LaunchPutGet
 
 
 class LaunchFragment : Fragment(R.layout.launch) {
 
-    lateinit var binding: LaunchBinding
-        private set
+    private lateinit var binding: LaunchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +25,21 @@ class LaunchFragment : Fragment(R.layout.launch) {
     ): View {
         binding = LaunchBinding.inflate(layoutInflater)
 
+        val data = arguments?.getSerializable(LaunchPutGet.LaunchKey.key) as? Launch
+        logExecutor(mes = "data = ${data}") // сюда приходят данные!!
+
+        binding.apply {
+            if (data != null) {
+                name.text = data.name
+
+                model.text = data.model
+
+                start.text = data.dateStart
+
+                icon.setImageResource(data.image)
+            }
+        }
         return inflater.inflate(R.layout.launch, container, false)
     }
 
-    fun setContent(){
-
-    }
 }
