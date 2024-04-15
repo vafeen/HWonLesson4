12e0156a385d0971c2dbde67fbd36ebc01cdc1cc
Vafeen <666av6@gmail.com>
2024-04-15 05:22:57 +0300

    Add image to Rocket.kt

--- app/src/main/java/ru/vafeen/hwonlesson4/ui/Rocket.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/ui/Rocket.kt
@@ -1,8 +1,12 @@
 package ru.vafeen.hwonlesson4.ui
 
+import androidx.annotation.DrawableRes
+
 data class Rocket(
 
     val name: String,
 
     val active: Boolean,
+
+    @DrawableRes val image: Int
 )
