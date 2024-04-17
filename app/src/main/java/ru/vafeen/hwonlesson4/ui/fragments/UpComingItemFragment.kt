3155f9fcdd40aa3b7994fdb1bee2799aa374eba7
Vafeen <666av6@gmail.com>
2024-04-17 20:56:44 +0300

    Fix UpComingItemFragment.kt

--- app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/UpComingItemFragment.kt
+++ app/src/main/java/ru/vafeen/hwonlesson4/ui/fragments/UpComingItemFragment.kt
@@ -6,11 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.vafeen.hwonlesson4.R
-import ru.vafeen.hwonlesson4.databinding.LaunchBinding
 import ru.vafeen.hwonlesson4.databinding.UpcomingInfoItemBinding
 import ru.vafeen.hwonlesson4.noui.logExecutor
 import ru.vafeen.hwonlesson4.ui.UpComingItem
-import ru.vafeen.hwonlesson4.ui.launch.Launch
 import ru.vafeen.hwonlesson4.ui.launch.LaunchPutGet
 
 class UpComingItemFragment : Fragment(R.layout.upcoming_info_item) {
